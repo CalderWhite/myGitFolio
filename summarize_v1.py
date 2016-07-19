46ee058255879fb55513976d70c13b6efa2433ad
Calder White <calderwhite1@gmail.com>
2016-07-19 15:28:41 -0400

    The last update for v1, v2 in progress

--- summarize_v1.py
+++ summarize_v1.py
@@ -1,3 +1,5 @@
+import urllib.request as request
+import json, base64
 def joinLines(arr,st,en):
     res = []
 
@@ -9,12 +11,15 @@ def summary(text):
     desc = "Error loading description."
     if text == None or text == "":
         desc = "No description available."
+    elif text == False:
+        desc = "Bad file type"
     elif text.find("#") < 0:
         if len(text) > 130:
             desc = text[0:130] + "..."
         else:
             desc = text
     else:
+        print("starting logic...")
         # don't just search for '#'s since it's less thorough
         rl = text.split("\n")
         if rl[-1] == "":
@@ -23,7 +28,7 @@ def summary(text):
         jse = []
         for i in rl:
             if i != "":
-                if i[0].find("#") > -1 or i[0].find("#") > -1:
+                if i[0].find("#") > -1 or i[0].find("-") > -1:
                     jse.append(rl.index(i))
                 if len(jse) >= 2:
                     break
@@ -59,5 +64,6 @@ def summary(text):
                     desc = j
     return desc
 if __name__ == '__main__':
-    mySumm = summary(b"# ImgShare, now with Mobile!\nFree, easy image sharing using c9.io\n## About\nThis is a little server written in python that I wrote up in a day    \nthat allows you to upload images without any account, or fees.    \nThis server is made to run on [cloud 9](http://c9.io). This is a website    \nthat essentially allows you to remotely control an unbuntu machine.    \nAs a cherry on top, c9 gives you teporary hosting (domain name) for free.    \nThe idea of this project was quick, free and easy. However, c9 does have premium accounts (similar to github and other cloud platforms.)\n## Links\nMy c9 project : https://ide.c9.io/calderwhite/imgshare    \nWhen running, this is the upload domain: https://imgshare-calderwhite.c9users.io\n## Usage\n### Backend (Terminal)\nFrom the backend all there is to do is run the file.\n#### Execution\nTo run, enter the imgshare directory and then run:    \n`python3 c9Server.py`\n### Browser (Client)\nFrom a Browser you can upload, and view (download) images.\n#### Viewing\nIn order to view an image from a web page (when server running), here's how:    \n`https://imgshare-<username>.c9users.io/userData/userFiles/<user's ip>/<filename>`\n#### Uploading\nUploading is rather simple, just go to the base domain:    \n`https://imgshare-<username>/c9users.io`".decode('utf-8'))
-    print(mySumm)
+    #myBytes = request.urlopen("https://raw.githubusercontent.com/harthur/brain/master/README.md").read().decode('utf-8')
+    #mySumm = summary(myBytes)
+    #print(mySumm)
